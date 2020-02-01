d1521c7060a707aa374cecbcb7abbe4bd692c67f
David R Connell <davidconnell12@gmail.com>
2020-02-01 14:29:03 -0600

    Fix: Remove end '/'

--- pathjoin.m
+++ pathjoin.m
@@ -2,6 +2,6 @@ function combinedPath = pathjoin(patha, pathb)
     if not(endsWith(patha, '/'))
         patha = [patha, '/'];
     end
-    pathb = strip(pathb, 'right', '/');
+    pathb = strip(pathb, 'left', '/');
     combinedPath = [patha pathb];
 end
