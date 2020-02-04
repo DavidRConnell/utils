3bf05628bd0be31a835e2795ba100e6664a30de1
David R Connell <davidconnell12@gmail.com>
2020-02-03 23:51:21 -0600

    Add: Allow for variable number of paths

--- pathjoin.m
+++ pathjoin.m
@@ -1,7 +1,12 @@
-function combinedPath = pathjoin(patha, pathb)
+function combinedPath = pathjoin(patha, pathb, varargin)
 %PATHJOIN combine parts of a path
-%   FULLPATH = PATHJOIN(PATHA, PATHB) return the full path PATHA/PATHB
-%   stripping extra path seperators at the boundary.
+%   FULLPATH = PATHJOIN(PATHA, PATHB) return the full path made from joining
+%   PATHA to PATHB stripping extra path seperators at the boundary.
+%   FULLPATH = PATHJOIN(PATHA, PATHB, PATH...) return the full path made from
+%   joining all arguments.
+
+    validateattributes(patha, {'char', 'string'}, {})
+    validateattributes(pathb, {'char', 'string'}, {})
 
     if not(endsWith(patha, '/'))
         patha = [patha, '/'];
@@ -9,4 +14,10 @@ function combinedPath = pathjoin(patha, pathb)
 
     pathb = strip(pathb, 'left', '/');
     combinedPath = [patha pathb];
+
+    if length(varargin) > 1
+        combinedPath = utils.pathjoin(combinedPath, varargin{1}, varargin{2:end});
+    elseif ~isempty(varargin)
+        combinedPath = utils.pathjoin(combinedPath, varargin{1});
+    end
 end
