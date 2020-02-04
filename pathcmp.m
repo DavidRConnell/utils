68de00211e7696e29c54e872f299e07e42d9187a
David R Connell <davidconnell12@gmail.com>
2020-02-03 22:40:04 -0600

    Docs: Add help section

--- pathcmp.m
+++ pathcmp.m
@@ -1,4 +1,8 @@
 function flag = pathcmp(patha, pathb)
+%PATHCMP compare to paths for equality
+%   PATHCMP(PATHA, PATHB) returns true if both paths are equivalant, after
+%   path expansion.
+
     flag = strcmp(expandpath(patha), expandpath(pathb));
 
     function fullpath = expandpath(path)
--- pathjoin.m
+++ pathjoin.m
@@ -1,7 +1,12 @@
 function combinedPath = pathjoin(patha, pathb)
+%PATHJOIN combine parts of a path
+%   FULLPATH = PATHJOIN(PATHA, PATHB) return the full path PATHA/PATHB
+%   stripping extra path seperators at the boundary.
+
     if not(endsWith(patha, '/'))
         patha = [patha, '/'];
     end
+
     pathb = strip(pathb, 'left', '/');
     combinedPath = [patha pathb];
 end
