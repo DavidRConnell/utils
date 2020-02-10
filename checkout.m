d6b263bc8474032b489cf3877ef542b3e278d119
David R Connell <davidconnell12@gmail.com>
2020-02-10 17:59:14 -0600

    Doc: add help block

--- checkout.m
+++ checkout.m
@@ -1,4 +1,9 @@
-function oldP = checkout(branch)
+function checkout(branch)
+%CHECKOUT switch package directory
+%   CHECKOUT(BRANCH) replace the current branch (i.e. subdir of
+%   ~/programs/MatlabPackages) from MATLAB's path with BRANCH, a different
+%   subdir.
+
     rootDir = utils.pathjoin(getenv('HOME'), '/programs/MatlabPackages/');
     assert(exist([rootDir branch], 'dir') == 7, 'Branch does not exist');
 
