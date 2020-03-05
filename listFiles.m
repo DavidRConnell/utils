3f1361eb6c29a055725397bfd6006e9a369a37ff
David R Connell <davidconnell12@gmail.com>
2020-03-05 15:18:41 -0600

    Clean: don't show hidden files by default
    But add option to show them if desired.

--- listFiles.m
+++ listFiles.m
@@ -1,15 +1,25 @@
-function list = listFiles(dirName)
+function list = listFiles(dirName, showHidden)
 %LISTFILES list all files in DIR
 %
 %   LIST = LISTFILES(DIR) return a cell array of all files in DIR. Accepts globs.
+%   LIST = LISTFILES(DIR, SHOWHIDDEN) if SHOWHIDDEN is true include hidden files.
+
+    if nargin < 2
+        showHidden = false;
+    end
 
     dirContents = dir(dirName);
     dirContents = {dirContents.name}';
-    list = removeNonFiles(dirContents);
+    list = removeNonFiles(dirContents, showHidden);
 
-    function list = removeNonFiles(dirContents)
-        list = dirContents(~strcmp(dirContents, '.') & ~strcmp(dirContents, '..'));
-        list = regexp(list, '^\.?.+\..*', 'once', 'match');
+    function list = removeNonFiles(dirContents, showHidden)
+        if showHidden
+            pattern = '^\.?[^\.]+\..*';
+        else
+            pattern = '^[^\.]+\..*';
+        end
+
+        list = regexp(dirContents, pattern, 'once', 'match');
         list = list(contains(list, '.'));
     end
 end
