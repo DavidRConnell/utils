e0ce20a814ddf97b9de785e1ec405b5edc99cd55
David R Connell <davidconnell12@gmail.com>
2020-03-05 12:25:10 -0600

    Fix: to work with file names with spaces

--- listFiles.m
+++ listFiles.m
@@ -1,13 +1,9 @@
-function list = listFiles(dir)
+function list = listFiles(dirName)
 %LISTFILES list all files in DIR
 %
 %   LIST = LISTFILES(DIR) return a cell array of all files in DIR. Accepts globs.
 
-    files = split(ls(dir));
-    files = files(~strcmp(files, ''));
-    list{length(files), 1} = '';
-    for i = 1:length(files)
-        [~, name, ext] = fileparts(files{i});
-        list{i} = [name ext];
-    end
+    files = dir(dirName);
+    list = {files.name}';
+    list = list(~strcmp(list, '.') & ~strcmp(list, '..'));
 end
